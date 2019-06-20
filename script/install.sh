e2093ccd34a82f7856bc3e81cb2364fa49e92b69
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-06-21 01:33:55 +0600

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -5,24 +5,20 @@ mkdir ~/.script
 export PATH="$PATH:~/.script"
 
 # Downloading the scripts
-echo "Downloading the scripts"
-sudo wget --quiet -O $HOME/.script/LoL https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/LoL &
+sudo wget --quiet -O $HOME/.script/LoL https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/LoL 
 sudo chmod +x $HOME/.script/LoL
-sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest &
+sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
 sudo chmod +x $HOME/.script/contest
 
 
 # Downloading Profile and Theme
-echo "Downloading Profile and Theme"
-sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc &
-sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim &
-
+sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc 
+sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim
 
 
 # Downloading Packages
-echo "Downloading Packages"
-sudo wget --quiet -O $HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/installation-list.sh &
+sudo wget --quiet -O $HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/installation-list.sh
 sudo chmod +x $HOME/.script/installation-list.sh
-installation-list.sh &
+installation-list.sh
 sudo mv $HOME/install.sh $HOME/.script/install.sh 
 echo "Done"
