9a49a008def1ce74aac3c2a4a104a2307f13010b
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-06-21 00:11:00 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,21 @@
 # Author : Kamrul
 
-# Moving to home directory
-cd ~/
+# Creating a hidden script directory
+mkdir ~/.script
+export PATH="$PATH:~/.script"
 
 #Checking if the user is root
-if [ "$(whoami)" != 'root' ]; then
-  echo "You have no permission to run as non-root user."
-  echo "Enter as Root :"
-fi
+#if [ "$(whoami)" != 'root' ]; then
+# echo "You have no permission to run as non-root user."
+# echo "Enter as Root :"
+#fi
 
-echo "___________________________________Installing packages_________________________________________"
-sudo wget -O installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/installation-list.sh
-sudo sh ./installation-list.sh
-echo "___________________________________Deleting packages_________________________________________"
-sudo rm installation-list.sh
+sudo wget -O -$HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/installation-list.sh
+chmod +x installation-list.sh
+installation-list.sh
 
-clear
-echo "___________________________________Configuring Bash_________________________________________"
-sudo wget -O .bashrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.bashrc
+#sudo wget -O --directory-prefix="$HOME" .bashrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.bashrc
 
-clear
-echo "___________________________________Configureing VIM Editor_________________________________________"
-sudo wget -O.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.vimrc
-cd //usr/share/vim/vim80/colors
-sudo wget -O molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/vim_colorscheme/molokai.vim
-sudo wget -O slate.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/vim_colorscheme/slate.vim
-sudo wget -O professional.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/vim_colorscheme/professional.vim
-cd ~/
+sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.vimrc
+sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/vim_colorscheme/molokai.vim
+
