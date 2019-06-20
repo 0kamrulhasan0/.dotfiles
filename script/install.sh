502dd3b07c0e17c16a80950896b54c74030b1185
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-06-21 01:31:41 +0600

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -4,21 +4,25 @@
 mkdir ~/.script
 export PATH="$PATH:~/.script"
 
-#Checking if the user is root
-#if [ "$(whoami)" != 'root' ]; then
-# echo "You have no permission to run as non-root user."
-# echo "Enter as Root :"
-#fi
+# Downloading the scripts
+echo "Downloading the scripts"
+sudo wget --quiet -O $HOME/.script/LoL https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/LoL &
+sudo chmod +x $HOME/.script/LoL
+sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest &
+sudo chmod +x $HOME/.script/contest
 
-sudo wget --quiet -O $HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/installation-list.sh
-chmod +x installation-list.sh
-installation-list.sh
 
-#sudo wget -O --directory-prefix="$HOME" .bashrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.bashrc
-
-sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/.vimrc
-sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/vim_colorscheme/molokai.vim
-mv $HOME/install.sh $HOME/.script/install.sh 
+# Downloading Profile and Theme
+echo "Downloading Profile and Theme"
+sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc &
+sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim &
 
 
 
+# Downloading Packages
+echo "Downloading Packages"
+sudo wget --quiet -O $HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/installation-list.sh &
+sudo chmod +x $HOME/.script/installation-list.sh
+installation-list.sh &
+sudo mv $HOME/install.sh $HOME/.script/install.sh 
+echo "Done"
