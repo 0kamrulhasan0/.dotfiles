90eb858d44e9ea0508ad9fd306867ed92f7f6bb6
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-06-21 02:46:11 +0600

    Update installation-list.sh

--- script/installation-list.sh
+++ script/installation-list.sh
@@ -3,11 +3,9 @@
 
 
 # Printing Some Detail
-sudo apt --assume-yes --quiet install neofetch cmatrix
+sudo apt-get --assume-yes --quiet install neofetch cmatrix
 clear
 neofetch
-
-sudo apt autoremove
 
 # Adding necessary Repository
  
@@ -15,16 +13,15 @@ sudo apt autoremove
 # Update, Upgrade, autoremove
 LoL 
 
-
 # Basic Tool
-sudo apt --assume-yes --quiet install man tree zip unzip ranger conky tint2 tlp
+sudo apt-get --assume-yes --quiet install man tree zip unzip ranger conky tint2 tlp
 
 # Programming languages and Similar
-#sudo apt --assume-yes --quiet install essesential-build python3 python3-pip php7.2-cli nodejs 
+#sudo apt-get --assume-yes --quiet install essesential-build python3 python3-pip php7.2-cli nodejs 
 # default-jre opendjdk-8-jdk
 
 # Other Packages
-sudo apt --assume-yes --quiet install tmux vim openssh-server git gdb valgrind curl net-tools snap wget make libx11-dev
+sudo apt-get --assume-yes --quiet install tmux vim openssh-server git gdb valgrind curl net-tools snap wget make libx11-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Python Packages
