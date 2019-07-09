8ccd193c6120a00c271ce09273b11a09e1b4dcf5
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-07-09 13:37:48 +0600

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -5,8 +5,6 @@ mkdir ~/.script
 export PATH="$PATH:$HOME/.script"
 
 # Downloading the scripts
-sudo wget --quiet -O $HOME/.script/LoL https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/LoL 
-sudo chmod +x $HOME/.script/LoL
 sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
 sudo chmod +x $HOME/.script/contest
 
@@ -17,8 +15,8 @@ sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubu
 
 
 # Downloading Packages
-sudo wget --quiet -O $HOME/.script/installation-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/installation-list.sh
-sudo chmod +x $HOME/.script/installation-list.sh
+sudo wget --quiet -O $HOME/.script/package-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/package-list.sh
+sudo chmod +x $HOME/.script/package-list.sh
 installation-list.sh
 sudo mv $HOME/install.sh $HOME/.script/install.sh 
 
