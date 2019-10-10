8ea99a086f97881452bd426a2a7b21f622d8521d
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-10-11 01:09:21 +0600

    Update package-list.sh

--- script/package-list.sh
+++ script/package-list.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 # VERSION=1.0
-
-
-# Printing Some Detail
-sudo apt-get --assume-yes --quiet install neofetch cmatrix
 clear
-neofetch
 
 # Adding necessary Repository
  
 
 # Update, Upgrade, autoremove
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update upgrade
 
 # Basic Tool
-sudo apt-get --assume-yes --quiet install ranger conky tint2 tlp
+#sudo apt-get --assume-yes --quiet install ranger conky tint2 tlp
 
 # Programming languages and Similar
 sudo apt-get --assume-yes --quiet install essesential-build python3 python3-pip nodejs 
 
 # Scientific Tool
-sudo apt-get --assume-yes --quiet install octave octave-gsl gsl-doc-info octave-doc gnuplot
-if [ ! -a ~/.octaverc ]; then echo 'graphics_toolkit("gnuplot")' > ~/.octaverc; fi
+#sudo apt-get --assume-yes --quiet install octave octave-gsl gsl-doc-info octave-doc gnuplot
+#if [ ! -a ~/.octaverc ]; then echo 'graphics_toolkit("gnuplot")' > ~/.octaverc; fi
 
 # Other Packages
-sudo apt-get --assume-yes --quiet install tmux vim openssh-server git gdb valgrind curl net-tools snap wget make libx11-dev
+sudo apt-get --assume-yes --quiet install tmux vim openssh-server git gdb wget make 
+#valgrind curl net-tools snap libx11-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Python Packages
-sudo pip3 install virtualenv
+pip3 install virtualenv
+pip3 install jupyter
+pip3 install requests
+pip3 install flask
 
 # Making shortcut for python 
 # Accessing it with py instead of python3
