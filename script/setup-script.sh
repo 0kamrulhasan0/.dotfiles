7e3a7cca06ab7162b63fa8758663d1217f771ba2
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2019-10-25 13:49:43 +0600

    Update setup-script.sh

--- script/setup-script.sh
+++ script/setup-script.sh
@@ -8,17 +8,17 @@ export PATH="$PATH:$HOME/.script"
 sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
 sudo chmod +x $HOME/.script/contest
 
-
-# Downloading Profile and Theme
-sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc 
-sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim
-
-
 # Downloading Packages
 sudo wget --quiet -O $HOME/.script/package-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/package-list.sh
 sudo chmod +x $HOME/.script/package-list.sh
 package-list.sh
 sudo mv $HOME/setup-script.sh $HOME/.script/setup-script.sh 
+
+# Downloading Profile and Theme
+sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc 
+sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim
+vim +PluginInstall +qall
+python3 ~/.vim/bundle/YouCompleteMe/install.py
 
 echo "Done"
 export PATH="$PATH:$HOME/.script"
