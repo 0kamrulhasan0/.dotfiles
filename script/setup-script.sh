90456144ed5794cc3e270449e4951bb1d73c25ad
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2020-06-08 10:10:02 +0600

    Update setup-script.sh

--- script/setup-script.sh
+++ script/setup-script.sh
@@ -5,8 +5,8 @@ mkdir ~/.script
 export PATH="$PATH:$HOME/.script"
 
 # Downloading the scripts
-sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
-sudo chmod +x $HOME/.script/contest
+#sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
+#sudo chmod +x $HOME/.script/contest
 
 # Downloading Packages
 sudo wget --quiet -O $HOME/.script/package-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/package-list.sh
@@ -17,7 +17,7 @@ sudo mv $HOME/setup-script.sh $HOME/.script/setup-script.sh
 # Downloading Profile and Theme
 sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc 
 sudo wget --quiet -O /usr/share/vim/vim80/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim
-vim +PluginInstall +qall
+sudo vim +PluginInstall +qall
 python3 ~/.vim/bundle/YouCompleteMe/install.py
 
 echo "Done"
