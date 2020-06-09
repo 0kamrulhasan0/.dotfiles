6b19400231df5e653924efe0703ab87dfaffc45b
0hasankamrul0 <32950339+0hasankamrul0@users.noreply.github.com>
2020-06-09 11:18:04 +0600

    Update setup-script.sh

--- script/setup-script.sh
+++ script/setup-script.sh
@@ -5,8 +5,8 @@ mkdir ~/.script
 export PATH="$PATH:$HOME/.script"
 
 # Downloading the scripts
-#sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
-#sudo chmod +x $HOME/.script/contest
+# sudo wget --quiet -O $HOME/.script/contest https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/contest
+# sudo chmod +x $HOME/.script/contest
 
 # Downloading Packages
 sudo wget --quiet -O $HOME/.script/package-list.sh https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/script/package-list.sh
@@ -18,8 +18,10 @@ sudo mv $HOME/setup-script.sh $HOME/.script/setup-script.sh
 sudo wget --quiet -O $HOME/.vimrc https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/.vimrc 
 sudo wget --quiet -O /usr/share/vim/vim81/colors/molokai.vim https://raw.githubusercontent.com/0hasankamrul0/My-Default-Settings/master/profile/molokai.vim
 vim +PluginInstall +qall
-python3 ~/.vim/bundle/YouCompleteMe/install.py
 
-echo "Done"
-export PATH="$PATH:$HOME/.script"
-echo "====================================================="
+cd ~/.vim/bundle/YouCompleteMe
+git submodule sync --recursive
+git submodule update --init --recursive
+python3 install.py
+
+echo "Done...!"
