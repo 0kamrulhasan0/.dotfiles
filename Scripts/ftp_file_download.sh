2a4d6ea82cdd992d5f0e1160f042b8302fbb33a1
Kamrul Hasan <hasankamrul2097@gmail.com>
2022-10-07 12:16:00 +0600

    Added single file download utility in ftp_download script

--- Scripts/ftp_file_download.sh
+++ Scripts/ftp_file_download.sh
@@ -11,11 +11,17 @@ download_all_from_page () {
   hxwls $link | grep -E $pattern | xargs -I % wget -c % -P $dir
 }
 
+download_single_file () {
+  link=$(echo $1 | sed 's/index.php?dir=//')
+  link=$(echo $link | sed 's/&file=.*//')
+  wget -c $link -P $dir_base
+}
 
 
 ##For seasons in one go:
 #for i in {01..10}; do
 #  #link=""
+#  link="http://172.22.22.103/ftpdata/tv_shows_eng/Curb%20Your%20Enthusiasm%20%5BComplete%5D/Season%2"$i"/"
 #  #echo $link
 #  download_all_from_page $link
 #done
